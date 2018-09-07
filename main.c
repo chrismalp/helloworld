5ba8c286a91b23deb6d474ba681d54a43641aab3
Chrismal Panditharathne <chrismal@innovware.com>
2018-09-07 11:02:52 +0530

    Added a bit of code to help ease merging.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ void myfunction ( void ) {
 	printf ("This is my-function!!!\n");
 }
 
+
+/* *** */
 int main(int argc, char* argv[]) {
 	printf("Hello World!   ver 1.3.0.1\n");
         myfunction();
