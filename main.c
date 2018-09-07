549c8a7ffb0cf7ed3b2142480a6ce5bc7ad18720
Chrismal Panditharathne <chrismal@innovware.com>
2018-09-07 07:36:00 +0530

    Added 'yourfunction' feature.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int yourfunction ( void ) {
+	printf ("Your Function!!!\n");
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
 	printf("Hello World!   ver 1.0.0.0\n");
