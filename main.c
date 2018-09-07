bc4cc5693aa9fa264afb1f3e4c59fb1ec4fd7b7e
Chrismal Panditharathne <chrismal@innovware.com>
2018-09-07 07:31:49 +0530

    Added 'myfunction' feature.

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void myfunction ( void ) {
+	printf ("This is my-function!!!\n");
+}
 
 int main(int argc, char* argv[]) {
 	printf("Hello World!   ver 1.0.0.0\n");
